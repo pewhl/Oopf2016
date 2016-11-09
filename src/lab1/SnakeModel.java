9aec9cacc399e59cda8a6d70e2858b6a1cd71566
Mikky88 <mikky.rowe@outlook.com>
2016-11-09 12:05:42 +0100

    Updated SnakeModel, new SnakeTile

--- src/lab1/SnakeModel.java
+++ src/lab1/SnakeModel.java
@@ -1,6 +1,11 @@
 package lab1;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by paula on 2016-11-08.
@@ -47,6 +52,11 @@ public class SnakeModel extends GameModel{
     /** Graphical representation of a blank tile. */
     private static final GameTile BLANK_TILE = new GameTile();
 
+    /** A list containing the positions of all coins. */
+    private final List<Position> coins = new ArrayList<Position>();
+
+    private final List<Position> snake = new LinkedList<Position>();
+
     /** The position of the collector. */
     private Position snakeHeadPos;
 
@@ -74,9 +84,107 @@ public class SnakeModel extends GameModel{
         setGameboardState(this.snakeHeadPos, SNAKEHEAD_TILE);
 //Ändrat fram tills hit---------------------------------------------------
         // Insert coins into the gameboard.
-        for (int i = 0; i < COIN_START_AMOUNT; i++) {
-            addCoin();
+        addCoin();
+
+    }
+
+    /**
+     * Insert another coin into the gameboard.
+     */
+    private void addCoin() {
+        Position newCoinPos;
+        Dimension size = getGameboardSize();
+        // Loop until a blank position is found and ...
+        do {
+            newCoinPos = new Position((int) (Math.random() * size.width),
+                    (int) (Math.random() * size.height));
+        } while (!isPositionEmpty(newCoinPos));
+
+        // ... add a new coin to the empty tile.
+        setGameboardState(newCoinPos, COIN_TILE);
+        this.coins.add(newCoinPos);
+    }
+
+    /**
+     * Return whether the specified position is empty.
+     *
+     * @param pos
+     *            The position to test.
+     * @return true if position is empty.
+     */
+    private boolean isPositionEmpty(final Position pos) {
+        return (getGameboardState(pos) == BLANK_TILE);
+    }
+
+    private boolean isPositionSnake(final Position pos) { return (getGameboardState(pos) == SNAKEBODY_TILE || getGameboardState(pos) == SNAKETAIL_TILE);}
+
+    /**
+     * Update the direction of the collector
+     * according to the user's keypress.
+     */
+    private void updateDirection(final int key) {
+        if (key == KeyEvent.VK_LEFT && direction != Directions.EAST) {
+            this.direction = Directions.WEST;
+        } else if (key == KeyEvent.VK_UP && direction != Directions.SOUTH) {
+            this.direction = Directions.NORTH;
+        } else if (key == KeyEvent.VK_RIGHT && direction != Directions.WEST) {
+            this.direction = Directions.EAST;
+        } else if (key == KeyEvent.VK_DOWN && direction != Directions.NORTH) {
+            this.direction = Directions.SOUTH;
         }
+    }
+
+    /**
+     * Get next position of the collector.
+     */
+    private Position getNextsnakeHeadPos() {
+        return new Position(
+                this.snakeHeadPos.getX() + this.direction.getXDelta(),
+                this.snakeHeadPos.getY() + this.direction.getYDelta());
+    }
+
+    /**
+     * This method is called repeatedly so that the
+     * game can update its state.
+     *
+     * @param lastKey
+     *            The most recent keystroke.
+     */
+    @Override
+    public void gameUpdate(final int lastKey) throws GameOverException {
+        updateDirection(lastKey);
+
+        // Erase the previous position.
+        setGameboardState(this.snakeHeadPos, SNAKEBODY_TILE);
+        // Change collector position.
+        this.snakeHeadPos = getNextsnakeHeadPos();
+
+        if (isOutOfBounds(this.snakeHeadPos)) {
+            throw new GameOverException(this.score);
+        }
+        if (isPositionSnake(this.snakeHeadPos)) {
+            throw new GameOverException(this.score);
+        }
+
+        // Draw collector at new position.
+        setGameboardState(this.snakeHeadPos, SNAKEHEAD_TILE);
+
+        // Remove the coin at the new collector position (if any)
+        if (this.coins.remove(this.snakeHeadPos)) {
+            this.score++;
+        }
+
+
+    }
+
+    /**
+     *
+     * @param pos The position to test.
+     * @return <code>false</code> if the position is outside the playing field, <code>true</code> otherwise.
+     */
+    private boolean isOutOfBounds(Position pos) {
+        return pos.getX() < 0 || pos.getX() >= getGameboardSize().width
+                || pos.getY() < 0 || pos.getY() >= getGameboardSize().height;
     }
 
 }
